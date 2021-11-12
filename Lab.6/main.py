63edb1ab52bc4b63571be0588588cd6da638fc66
PavloYend <pavlo.yendyk.knm.2020@lpnu.ua>
2021-11-12 14:27:14 +0200

    add.Lab7

--- Lab.6/main.py
+++ Lab.6/main.py
@@ -1,30 +1,257 @@
-from flask import Flask
-from models import User, Event, Session
+from flask import Flask, Response
+from flask_bcrypt import Bcrypt
+from models import *
+from flask import jsonify
+import json
+from flask import make_response
+from flask import request
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import insert
+
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-user1 = User(id=1, username="Vasyl Kovalchuk", firstName="Vasyl", lastName="Kovalchuk", email="vaskov@gmail.com", password="123", phone="093000")
-event1 = Event(id=1, name='Birthday', owner_id=1)
 
-user2 = User(id=2, username="Ivan M", firstName="Ivan", lastName="M", email="Iv@gmail.com", password="1000", phone="0978563")
-event2 = Event(id=2, name='Wedding', owner_id=2)
+def to_json(inst, cls):
+    """
+    Jsonify the sql alchemy query result.
+    """
+    convert = dict()
+    d = dict()
+    for c in cls.__table__.columns:
+        v = getattr(inst, c.name)
+        if c.type in convert.keys() and v is not None:
+            try:
+                d[c.name] = convert[c.type](v)
+            except:
+                d[c.name] = "Error:  Failed to covert using ", str(convert[c.type])
+        elif v is None:
+            d[c.name] = str()
+        else:
+            d[c.name] = v
+    return json.dumps(d)
 
-user3 = User(id=3, username="Petro B", firstName="Petro", lastName="B", email="petr@gmail.com", password="1111", phone="0994123")
-event3 = Event(id=3, name='Birthday', owner_id=3)
 
-with Session() as session:
-    session.add(user1)
-    session.commit()
-    session.add(event1)
-    session.commit()
+@app.route('/user', methods=['POST'])
+def create_user():
+    print("create_user")
+    user = User(
+        username=request.json.get('username'),
+        firstName=request.json.get('firstName'),
+        lastName=request.json.get('lastName'),
+        email=request.json.get('email'),
+        phone=request.json.get('phone'),
+        password=bcrypt.generate_password_hash(request.json.get('password')).decode('utf-8')
+    )
+    tvins = (Session.query(User).filter_by(username=user.username).all())
+    if tvins != []:
+        return make_response(jsonify({'error': 'Username is busy'}), 404)
+    try:
+        Session.add(user)
+        Session.commit()
+    except IntegrityError:
+        print('Incorrect data')
+        return make_response(jsonify({'error': 'Incorrect data'}), 404)
+    # tasks.append(task)
+    a = to_json(user, User)
+    return Response(response=a, status=200, mimetype="application/json")
 
-    session.add(user2)
-    session.commit()
-    session.add(event2)
-    session.commit()
 
-    session.add(user3)
-    session.commit()
-    session.add(event3)
-    session.commit()
-print(session.query(User).all())
+@app.route("/user/<int:id>", methods=['GET'])
+def get_user(id):
+    try:
+        a = to_json(Session.query(User).filter_by(id=id).one(), User)
+        return Response(response=a, status=200, mimetype="application/json")
+    except:
+        return make_response(jsonify({'error': 'User not found'}), 404)
+
+
+@app.route('/user/<int:id>', methods=['PUT'])
+def update_user(id):
+    print("update_user")
+    u = Session.query(User).filter_by(id=id).one()
+    if not u:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    if request.json.get('username'):
+        tvins = (Session.query(User).filter_by(username=request.json.get('username')).all())
+        if tvins != []:
+            return make_response(jsonify({'error': 'username is busy'}), 409)
+        u.username = request.json.get('username')
+    if request.json.get('firstName'):
+        u.firstName = request.json.get('firstName')
+    if request.json.get('lastName'):
+        u.lastName = request.json.get('lastName')
+    if request.json.get('email'):
+        u.email = request.json.get('email')
+    if request.json.get('phone'):
+        u.phone = request.json.get('phone')
+    if request.json.get('password'):
+        u.password = request.json.get('password')
+    Session.commit()
+
+    return Response(response=to_json(u, User), status=200, mimetype="application/json")
+
+
+@app.route('/user/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    try:
+        user = Session.query(User).filter_by(id=id).first()
+        Session.delete(user)
+        Session.commit()
+        return {
+            "msg": "User deleted successfully",
+            "id": id
+        }
+    except:
+        return "User not found", 404
+
+
+@app.route('/event', methods=['POST'])
+def create_event():
+    print("create_event")
+    event = Event(
+        name=request.json.get('name'),
+        owner_id=request.json.get('owner_id'),
+    )
+    try:
+        Session.add(event)
+        Session.commit()
+    except IntegrityError:
+        print('Incorrect data')
+        return make_response(jsonify({'error': 'Incorrect data'}), 409)
+    # tasks.append(task)
+    a = to_json(event, Event)
+    return Response(response=a, status=200, mimetype="application/json")
+
+
+@app.route('/event/<int:id>', methods=['PUT'])
+def update_event(id):
+    print("Update_event")
+    u = Session.query(Event).filter_by(id=id).all()
+    if not u:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    if request.json.get('name'):
+        u.name = request.json.get('name')
+    if request.json.get('owner_id'):
+        u.owner_id = request.json.get('owner_id')
+
+    Session.commit()
+    return Response(response=to_json(u, Event), status=200, mimetype="application/json")
+
+
+@app.route("/event/<int:id>", methods=['GET'])
+def get_event(id):
+    try:
+        a = to_json(Session.query(Event).filter_by(id=id).one(), Event)
+        return Response(response=a, status=200, mimetype="application/json")
+    except:
+        return make_response(jsonify({'error': 'Event not found'}), 404)
+
+
+@app.route('/event/<int:id>', methods=['DELETE'])
+def delete_event(id):
+    try:
+        event = Session.query(Event).filter_by(id=id).first()
+        Session.delete(event)
+        Session.commit()
+        return {
+            "msg": "Event deleted successfully",
+            "id": id
+        }
+    except:
+        return "Event not found", 404
+
+
+@app.route('/event/<int:eve_id>/user/<int:use_id>', methods=['POST'])
+def request_user(eve_id, use_id):
+    k = Session.query(user_event).filter_by(user_id=use_id, event_id=eve_id).all()
+    if k != []:
+        return make_response(jsonify({'error': 'request already exist'}), 404)
+
+    u = Session.query(User).filter_by(id=use_id).all()
+    if not u:
+        return make_response(jsonify({'error': 'User Not found'}), 404)
+    q = Session.query(Event).filter_by(id=eve_id).all()
+    if not q:
+        return make_response(jsonify({'error': ' Event Not found'}), 404)
+
+    stmt = (
+        insert(user_event).values(user_id=use_id, event_id=eve_id)
+    )
+    try:
+        connect.execute(stmt);
+    except:
+        return make_response(jsonify({'Error': 'Not found'}), 404)
+
+    return make_response(jsonify({'Successful': 'operation'}), 200)
+
+
+@app.route('/your_events/user/<int:id>', methods=['GET'])
+def your_events(id):
+
+    u = Session.query(User).filter_by(id=id).all()
+    if not u:
+        return make_response(jsonify({'Error': 'Not found'}), 404)
+    a = Session.query(Event).filter_by(owner_id=id).all()
+    json_data = []
+    for i in a:
+        json_data.append(to_json(i, Event))
+    try:
+        Session.commit()
+    except:
+        return make_response(jsonify({'Error': 'Not found'}), 404)
+
+    return Response(response=str(json_data), status=200, mimetype="application/json")
+
+
+@app.route('/event/<int:id>/users', methods=['GET'])
+def get_all_requested(id):
+
+    u = Session.query(Event).filter_by(id=id).all()
+    if not u:
+        return make_response(jsonify({'Error': 'Not found'}), 404)
+    a = Session.query(user_event).filter_by(event_id=id).all()
+
+    b = []
+    for i in a:
+        b.append(Session.query(User).filter_by(id=i.user_id).one())
+
+    json_data = []
+    for i in b:
+        json_data.append(to_json(i, User))
+    try:
+        Session.commit()
+    except:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    return Response(response=str(json_data), status=200, mimetype="application/json")
+
+
+@app.route('/events/user/<int:id>', methods=['GET'])
+def user_for_request(id):
+
+    u = Session.query(User).filter_by(id=id).all()
+    if not u:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    a = Session.query(user_event).filter_by(user_id=id).all()
+    b = []
+    for i in a:
+        b.append(Session.query(Event).filter_by(id=i.event_id).one())
+
+    json_data = []
+    for i in b:
+        json_data.append(to_json(i, Event))
+    try:
+        Session.commit()
+    except:
+        return make_response(jsonify({'error': 'Not found'}), 404)
+
+    return Response(response=str(json_data), status=200, mimetype="application/json")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
